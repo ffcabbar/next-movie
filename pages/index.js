b54bc695ff4cbc9aae293039f62b26eb7ad59ac4
ffcabbar <ffcabbar@gmail.com>
2021-01-02 23:35:55 +0300

    next.js added

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          My <a href="https://nextjs.org">Next.js</a> Project!
         </h1>
 
         <p className={styles.description}>
