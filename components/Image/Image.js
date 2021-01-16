2bc7adfb580fdaaf749c47e7c2f5c20890097ac6
ffcabbar <ffcabbar@gmail.com>
2021-01-16 22:08:53 +0300

    Header and Image section updated.

--- components/Image/Image.js
+++ components/Image/Image.js
@@ -4,7 +4,23 @@ import styles from "./Image.module.scss";
 const Image = () => {
   return (
     <div className={styles.container}>
-      <h1>Image Overlay</h1>
+      <div className={styles.innerSection}>
+        <div>
+          <h1>
+            Welcome to <strong>MovieUP.</strong>
+          </h1>
+        </div>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris{" "}
+          </p>
+        </div>
+      </div>
+      <div className={styles.imagePoints}>
+        <img src="/image-points.svg" alt="Image Points"/>
+      </div>
       <div className={styles.colorOverlay}></div>
     </div>
   );
