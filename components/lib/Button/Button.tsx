2a5b7aea27180d1e4336a68bed3107df4009331f
ffcabbar <ffcabbar@gmail.com>
2022-01-02 23:14:08 +0300

    adds button component and classnames package

--- components/lib/Button/Button.tsx
+++ components/lib/Button/Button.tsx
@@ -1,14 +1,73 @@
 import React from 'react';
-
+import cn from 'classnames';
+import styles from './Button.module.scss';
 export interface IButtonProps
   extends React.DetailedHTMLProps<
-      React.ButtonHTMLAttributes<HTMLButtonElement>,
-      HTMLButtonElement
-    >,
-    React.AriaAttributes {}
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  > {
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+  leftIcon?: React.ReactElement;
+  rightIcon?: React.ReactElement;
+}
 
-export const ButtonPrimary: React.FC<IButtonProps> = (props) => {
-  const { children, ...rest } = props;
+type ButtonSize = 'xs' | 'sm' | 'md' | 'lg';
+type ButtonVariant = 'solid' | 'outline' | 'ghost';
 
-  return <button {...rest}>{children}</button>;
+export const Button: React.FC<IButtonProps> = (props) => {
+  const {
+    children = 'Button',
+    style,
+    className,
+    size = 'md',
+    variant = 'solid',
+    leftIcon,
+    rightIcon,
+    ...rest
+  } = props;
+
+  let _style: React.CSSProperties = style || {};
+
+  /* Override Defaults */
+  // if (color && _style) {
+  //   _style.color = color;
+  // }
+
+  let buttonSize;
+  if (size === 'xs') {
+    buttonSize = styles.buttonXs;
+  } else if (size === 'sm') {
+    buttonSize = styles.buttonSm;
+  } else if (size === 'md') {
+    buttonSize = styles.buttonMd;
+  } else {
+    buttonSize = styles.buttonLg;
+  }
+
+  let buttonVariant;
+  if (variant === 'solid') {
+    buttonVariant = styles.buttonSolid;
+  } else if (variant === 'outline') {
+    buttonVariant = styles.buttonOutline;
+  } else if (variant === 'ghost') {
+    buttonVariant = styles.buttonGhost;
+  }
+
+  return (
+    <button
+      className={cn(
+        styles.button,
+        `${buttonSize}`,
+        `${buttonVariant}`,
+        className
+      )}
+      style={_style}
+      {...rest}
+    >
+      {leftIcon && <span>{leftIcon}</span>}
+      {children}
+      {rightIcon && <span>{rightIcon}</span>}
+    </button>
+  );
 };
