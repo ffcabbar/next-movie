c025878dd19c75204d7e7d38743feba45dc4276c
ffcabbar <ffcabbar@gmail.com>
2022-01-16 00:18:39 +0300

    adds size prop for input component

--- components/lib/Input/Input.tsx
+++ components/lib/Input/Input.tsx
@@ -6,16 +6,35 @@ export interface IInputProps
   extends React.DetailedHTMLProps<
     React.InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
-  > {}
+  > {
+  sizeOfInput?: InputSize;
+}
+
+type InputSize = 'xs' | 'sm' | 'md' | 'lg';
 
 export const Input: React.FC<IInputProps> = (props) => {
-  const { style, className, ...rest } = props;
+  const { style, className, sizeOfInput = 'md', ...rest } = props;
 
   let _style: React.CSSProperties = style || {};
 
+  let inputSize;
+  if (sizeOfInput === 'xs') {
+    inputSize = styles.inputXs;
+  } else if (sizeOfInput === 'sm') {
+    inputSize = styles.inputSm;
+  } else if (sizeOfInput === 'md') {
+    inputSize = styles.inputMd;
+  } else {
+    inputSize = styles.inputLg;
+  }
+
   return (
     <>
-      <input className={cn(styles.input, className)} style={_style} {...rest} />
+      <input
+        className={cn(styles.input, `${inputSize}`, className)}
+        style={_style}
+        {...rest}
+      />
     </>
   );
 };
