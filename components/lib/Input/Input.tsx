ba4513889bd6e12a04c6ff3a22eff5c8b3585e99
ffcabbar <ffcabbar@gmail.com>
2022-01-20 01:56:28 +0300

    adds icon prop for input component

--- components/lib/Input/Input.tsx
+++ components/lib/Input/Input.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 import cn from 'classnames';
 import styles from './Input.module.scss';
 
-export interface IInputProps
-  extends Omit<
-    React.DetailedHTMLProps<
-      React.InputHTMLAttributes<HTMLInputElement>,
-      HTMLInputElement
-    >,
-    'size'
-  > {
+export interface IInputProps extends Omit<A, 'size'> {
   size?: InputSize;
+  icon?: React.ReactElement;
 }
 
+type A = React.DetailedHTMLProps<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+>;
 type InputSize = 'xs' | 'sm' | 'md' | 'lg';
 
 export const Input: React.FC<IInputProps> = (props) => {
-  const { style, className, size = 'md', ...rest } = props;
+  const { style, className, size = 'md', icon, ...rest } = props;
 
   let _style: React.CSSProperties = style || {};
 
@@ -32,12 +30,13 @@ export const Input: React.FC<IInputProps> = (props) => {
   }
 
   return (
-    <>
+    <div className={styles.wrapper}>
       <input
         className={cn(styles.input, `${inputSize}`, className)}
         style={_style}
         {...rest}
       />
-    </>
+      {icon && <span>{icon}</span>}
+    </div>
   );
 };
