88b9622b4d09705ec5ebdd881feea209c339024e
ffcabbar <ffcabbar@gmail.com>
2022-01-22 16:25:58 +0300

    fixes typo and remove comment

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import Header from '../Header/header';
 import Footer from '../Footer/Footer';
-import React from 'react';
 
 type Props = {
   children?: React.ReactNode;
@@ -10,11 +10,7 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Header />
-      <main>
-        {/* <section style={{ width: '1024px' }}>{children}</section> */}
-        {/* <SideBar /> */}
-        {children}
-      </main>
+      <main>{children}</main>
       <Footer />
     </>
   );
